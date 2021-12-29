124bd3755017f15124d25a9a2b255526b24dd3e9
artewka <33059920+artewka@users.noreply.github.com>
2021-12-29 02:01:22 +0200

    Update providers.tf

--- Terraform_modules/providers.tf
+++ Terraform_modules/providers.tf
@@ -14,14 +14,14 @@ terraform {
 
 
 provider "aws" {
-   access_key = "AKIAZJUOCNDEJLKVZPER"
-   secret_key = "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"
+   access_key = ""
+   secret_key = ""
    region     = "eu-central-1"
 }
 
 
 provider "cloudflare" {
-   email = "shadak1997@gmail.com"
-   api_key = "cce4a5bb0b1db178ca20808e43a3ef130dcd5"
+   email = ""
+   api_key = ""
    version = "~> 3.0"
 }
